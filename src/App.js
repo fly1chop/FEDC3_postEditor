f1268348e887a51336651fdb8c1a14814f5c97b2
Jane Ro <janero95@gmail.com>
2022-11-08 15:42:05 +0900

    refactor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import PostsPage from "./components/PostsPage.js";
+import PostEditPage from "./components/PostEditPage.js";
+
+// url 규칙
+// root: postPage 그리기
+
+// `/posts/${id}` - id에 해당하는 post 생성
+// `/posts/new` - 새 post 생성
 
 export default function App({ $target }) {
+  const postsPage = new PostsPage({ $target });
+  const postEditPage = new PostEditPage({
+    $target,
+    initialState: {
+      postId: "new",
+      post: {
+        title: '',
+        content: ''
+      }
+    },
+  });
 
-  const postsPage = new PostsPage({ $target })
+  this.route = () => {
+    const { pathname } = window.location;
 
-  postsPage.render()
+    if (pathname === "/") {
+      postsPage.render();
+    } else if (pathname.indexOf("/posts/") === 0) {
+      const [, , postId] = pathname.split("/");
+      postEditPage.setState({ postId });
+    }
+  };
+
+  this.route();
 }
